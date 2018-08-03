f3269992fb70ed54cc413ec411fd9031686dc9f9
dika <dikabocah@gmail.com>
2018-08-04 00:55:04 +0700

    qty filter

--- src/pages/note-order/note-order.ts
+++ src/pages/note-order/note-order.ts
@@ -8,8 +8,14 @@ import { NavParams , ViewController } from 'ionic-angular';
 })
 export class NoteOrderPage {
   dt : any ;
+  jadi = {
+    'barang_id' : "",
+    'qty' : ""
+    }
   constructor( public viewCtrl : ViewController , public navParams: NavParams) {
     this.dt = this.navParams.get('barang');
+    this.jadi.qty = this.dt.qty;
+    this.jadi.barang_id = this.dt.barang_id;
   }
 
   ionViewDidLoad() {
@@ -18,7 +24,7 @@ export class NoteOrderPage {
 
   simpanNote(){
     if(this.dt.qty >= 1 ){
-      this.viewCtrl.dismiss(this.dt);
+      this.viewCtrl.dismiss(this.jadi);
     }else{
       alert('Qty Tidak Boleh kurang dari 1 !');
     }
